525c063fb7597709b43d272fb3a09e5e1dfda13d
artemis <artemis.yanez@gmail.com>
2022-04-21 12:40:55 -0700

    The entire setup for the app it uses fragment navigation and recycler view for the menu, and events.  I will setup the adapter classes needed to get the menu items to display in the app.

---
+++ app/src/main/java/artemis/yanez/jarrodcoffee/fragments/JarrodMenuFragment.kt
@@ -0,0 +1,23 @@
+package artemis.yanez.jarrodcoffee.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import artemis.yanez.jarrodcoffee.R
+
+
+class JarrodMenuFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_jarrod_menu, container, false)
+    }
+
+
+}
