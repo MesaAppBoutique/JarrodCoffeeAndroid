39288daabed66ae0a1fd336aee71e132e0467c79
elmomoloco7890 <thegreek.god@gmail.com>
2022-06-01 11:43:15 -0700

    attempted to change the gradle build properties due to an issue of updating versions.
    updated the main activity to display the home fragment.

--- app/src/main/java/artemis/yanez/jarrodcoffee/MainActivity.kt
+++ app/src/main/java/artemis/yanez/jarrodcoffee/MainActivity.kt
@@ -2,10 +2,26 @@ package artemis.yanez.jarrodcoffee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.jarrod_coffee_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
+        //setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
