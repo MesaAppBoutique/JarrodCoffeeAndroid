5ee13ad27d258d0dccdfae255230e821d3fab1bb
elmomoloco7890 <thegreek.god@gmail.com>
2022-06-03 13:53:59 -0700

    attempted to change the gradle build properties due to an issue of updating versions.
    updated the main activity to display the home fragment. Created a new file for a viewmodel to make it easier for function and data to be pushed

--- app/src/main/java/artemis/yanez/jarrodcoffee/fragments/JarrodHomeFragment.kt
+++ app/src/main/java/artemis/yanez/jarrodcoffee/fragments/JarrodHomeFragment.kt
@@ -6,17 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import artemis.yanez.jarrodcoffee.R
+import artemis.yanez.jarrodcoffee.databinding.FragmentJarrodHomeBinding
 
 
 class JarrodHomeFragment : Fragment() {
+
+    //binding the layout fragment xml
+    private var binding: FragmentJarrodHomeBinding? = null
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jarrod_home, container, false)
+    ): View {
+       /* // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_jarrod_home, container, false)*/
+        val fragmentBinding = FragmentJarrodHomeBinding.inflate(inflater, container, false)
+        return fragmentBinding.root
     }
 
 
